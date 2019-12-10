71e57c18d961635b0aede83877b81a5a3dd1580e
Maël Querré <mael.querre@gmail.com>
2019-12-11 00:26:39 +0100

    Fix intakes state init

--- components/MealsView.js
+++ components/MealsView.js
@@ -12,23 +12,9 @@ class MealsView extends React.Component {
     }
 
     days.forEach(day => {
+      this.state.intakes[day] = {}
       meals.forEach(meal => {
-        // if (!(day in this.state.intakes)) {
-        //   this.setState({
-        //     intakes: {
-        //       ...this.state.intakes,
-        //       monday: {}
-        //     }
-        //   })
-        // }
-        console.log(this.state.intakes)
-        // this.setState({
-        //   intakes: {
-        //     [day]: {
-        //       [meal]: { portions: 0 }
-        //     }
-        //   }
-        // })
+        this.state.intakes[day][meal] = { portions: 0 }
       })
     })
 
@@ -61,57 +47,57 @@ class MealsView extends React.Component {
           })}
         </nav>
 
-        {/*<div className="grid columns-3 gap-5" style={{ gridTemplateColumns: 'max-content 1fr 1fr' }}>*/}
-        {/*  {meals.map((meal, index) => {*/}
-        {/*    return <div key={index} style={{ display: 'contents' }}>*/}
-        {/*      <h2 className="text-lg font-semibold">{meal.charAt(0).toUpperCase() + meal.slice(1)}</h2>*/}
+        <div className="grid columns-3 gap-5" style={{ gridTemplateColumns: 'max-content 1fr 1fr' }}>
+          {meals.map((meal, index) => {
+            return <div key={index} style={{ display: 'contents' }}>
+              <h2 className="text-lg font-semibold">{meal.charAt(0).toUpperCase() + meal.slice(1)}</h2>
 
-        {/*      <div>*/}
-        {/*        <label htmlFor={`foodGroup${index}`}*/}
-        {/*               className="block mb-2 text-sm uppercase">*/}
-        {/*          Food group*/}
-        {/*        </label>*/}
-        {/*        <div className="relative">*/}
-        {/*          <select className="select"*/}
-        {/*                  id={`foodGroup${index}`}*/}
-        {/*                  defaultValue={'default'}>*/}
-        {/*            <option disabled hidden value="default">Choose a food group</option>*/}
-        {/*            {foodGroups.map((foodGroup, index) => {*/}
-        {/*              return <option key={index}>{foodGroup.name}</option>*/}
-        {/*            })}*/}
-        {/*          </select>*/}
-        {/*          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">*/}
-        {/*            <ChevronDown />*/}
-        {/*          </div>*/}
-        {/*        </div>*/}
-        {/*      </div>*/}
-        {/*      <div>*/}
-        {/*        <label htmlFor={`portion${index}`} className="block mb-2 text-sm uppercase">*/}
-        {/*          Portion*/}
-        {/*        </label>*/}
-        {/*        <div className="custom-number-input h-10 w-32">*/}
-        {/*          <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">*/}
-        {/*            <button onClick={() => this.updatePortion(meal, -1)}*/}
-        {/*                    data-action="decrement"*/}
-        {/*                    className="bg-gray-200 text-gray-700 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer focus:outline-none focus:shadow-outline">*/}
-        {/*              <Minus className="m-auto" size={16} />*/}
-        {/*            </button>*/}
-        {/*            <input type="number appearance-none"*/}
-        {/*                   className="text-center w-full bg-gray-200 text-md md:text-basecursor-default flex items-center text-gray-700 hover:text-black focus:outline-none focus:text-black"*/}
-        {/*                   name={`portion${index}`}*/}
-        {/*                   id={`portion${index}`}*/}
-        {/*                   value={this.state.intakes[this.state.currentDay][meal].portions} />*/}
-        {/*            <button onClick={() => this.updatePortion(meal, 1)}*/}
-        {/*                    data-action="increment"*/}
-        {/*                    className="bg-gray-200 text-gray-700 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer focus:outline-none focus:shadow-outline">*/}
-        {/*              <Plus className="m-auto" size={16} />*/}
-        {/*            </button>*/}
-        {/*          </div>*/}
-        {/*        </div>*/}
-        {/*      </div>*/}
-        {/*    </div>*/}
-        {/*  })}*/}
-        {/*</div>*/}
+              <div>
+                <label htmlFor={`foodGroup${index}`}
+                       className="block mb-2 text-sm uppercase">
+                  Food group
+                </label>
+                <div className="relative">
+                  <select className="select"
+                          id={`foodGroup${index}`}
+                          defaultValue={'default'}>
+                    <option disabled hidden value="default">Choose a food group</option>
+                    {foodGroups.map((foodGroup, index) => {
+                      return <option key={index}>{foodGroup.name}</option>
+                    })}
+                  </select>
+                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                    <ChevronDown />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <label htmlFor={`portion${index}`} className="block mb-2 text-sm uppercase">
+                  Portion
+                </label>
+                <div className="custom-number-input h-10 w-32">
+                  <div className="flex flex-row h-10 w-full rounded-lg relative bg-transparent mt-1">
+                    <button onClick={() => this.updatePortion(meal, -1)}
+                            data-action="decrement"
+                            className="bg-gray-200 text-gray-700 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer focus:outline-none focus:shadow-outline">
+                      <Minus className="m-auto" size={16} />
+                    </button>
+                    <input type="number appearance-none"
+                           className="text-center w-full bg-gray-200 text-md md:text-basecursor-default flex items-center text-gray-700 hover:text-black focus:outline-none focus:text-black"
+                           name={`portion${index}`}
+                           id={`portion${index}`}
+                           value={this.state.intakes[this.state.currentDay][meal].portions} />
+                    <button onClick={() => this.updatePortion(meal, 1)}
+                            data-action="increment"
+                            className="bg-gray-200 text-gray-700 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer focus:outline-none focus:shadow-outline">
+                      <Plus className="m-auto" size={16} />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          })}
+        </div>
       </div>
     )
   }
