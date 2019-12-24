d07deb1d8f598664cb741259ac01d6c9994ed30c
Maël Querré <mael.querre@gmail.com>
2019-12-24 12:31:06 +0100

    Improve responsive

--- components/NewIntakeCard.js
+++ components/NewIntakeCard.js
@@ -30,34 +30,34 @@ class NewIntakeCard extends React.Component {
         <img src={`/images/food/${this.state.foodId}.png`}
              className="self-center w-1/6 md:w-1/2 mr-4 md:mr-0 md:mt-4 md:mb-8" />
 
-        <div className="relative md:mb-4 h-10">
-          <select className="select"
-                  value={this.state.foodId}
-                  onChange={this.updateSelection}>
-            {foodGroups.map((foodGroup, index) => {
-              return <option key={index} value={foodGroup.id}>{foodGroup.name}</option>
-            })}
-          </select>
-          <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-            <ChevronDown size={18} />
+        <div className="flex flex-col mr-3 md:mr-0">
+          <div className="relative md:mb-4 h-10">
+            <select className="select"
+                    value={this.state.foodId}
+                    onChange={this.updateSelection}>
+              {foodGroups.map((foodGroup, index) => {
+                return <option key={index} value={foodGroup.id}>{foodGroup.name}</option>
+              })}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              <ChevronDown size={18} />
+            </div>
           </div>
-        </div>
 
-        <div className="flex-shrink-0 h-8 md:mb-6">
-          <div className="flex flex-col md:flex-row h-full w-full relative mt-1">
+          <div className="relative flex-shrink-0 flex h-8 mt-1 md:mb-6">
             <button onClick={() => this.updateFoodPortion(-1)}
                     onMouseDown={event => event.preventDefault()}
-                    className="h-full px-2 text-gray-700 bg-white md:hover:bg-gray-200 rounded-b md:rounded-l-lg cursor-pointer order-3 md:order-1">
+                    className="h-full px-2 text-gray-700 bg-white md:hover:bg-gray-200 rounded-l cursor-pointer">
               <Minus className="m-auto" size={16} />
             </button>
 
-            <span className="flex items-center justify-center px-4 md:px-0 text-center w-full bg-white text-gray-700 order-2">
-              {this.state.foodPortions} {this.state.foodPortions > 1 ? 'portions' : 'portion'}
+            <span className="flex items-center justify-center text-center w-full bg-white text-gray-700">
+                {this.state.foodPortions}&nbsp;{this.state.foodPortions > 1 ? 'portions' : 'portion'}
             </span>
 
             <button onClick={() => this.updateFoodPortion(1)}
                     onMouseDown={event => event.preventDefault()}
-                    className="h-full px-2 text-gray-700 bg-white md:hover:bg-gray-200 rounded-t md:rounded-r-lg cursor-pointer order-1 md:order-3">
+                    className="h-full px-2 text-gray-700 bg-white md:hover:bg-gray-200 rounded-r cursor-pointer">
               <Plus className="m-auto" size={16} />
             </button>
           </div>
@@ -70,7 +70,7 @@ class NewIntakeCard extends React.Component {
                 })}>
           Add
         </button>
-        <button className="md:hidden focus:outline-none"
+        <button className="md:hidden self-center focus:outline-none"
                 onClick={() => this.props.addIntake({
                   foodGroupId: this.state.foodId,
                   portions: this.state.foodPortions
